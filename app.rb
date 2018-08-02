077c27d1a7d90ceec8f57fcb2d1b58696222ce25
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-02 14:40:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/piglatinizer' do
+  post '/piglatinize' do
     @sentence = PigLatinizer.new(params["string"])
     erb :piglatinize
   end
