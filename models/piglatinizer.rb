441b42f73efe32bb69858b0557a838946906cc46
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-02 15:59:52 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,28 +3,27 @@ class PigLatinizer
   attr_reader :words
 
   def piglatinize(string)
-    sentence = []
-  string.split(" ").map do |word|
-    if !string[0].scan(/[aeiouAEIOU]/).empty?
-      string << "way"
-    elsif !string[0].scan(/[bcdfghjklmnpqrstvwxyz]/).empty? && !string[1].scan(/[aeiou]/).empty?
-      string << string[0]
-      string[0] = ""
-      string << "ay"
-    elsif !string[0].scan(/[bcdfghjklmnpqrstvwxyz]/).empty? && !string[1].scan(/[bcdfghjklmnpqrstvwxyz]/).empty? && !string[2].scan(/[bcdfghjklmnpqrstvwxyz]/).empty?
-      string << string[0]
-      string << string[1]
-      string << string[2]
-      string << "ay"
-      string[0] = ""
-      string[0] = ""
-      string[0] = ""
-    elsif !string[0].scan(/[bcdfghjklmnpqrstvwxyz]/).empty? && !string[1].scan(/[bcdfghjklmnpqrstvwxyz]/).empty?
-      string << string[0]
-      string << string[1]
-      string << "ay"
-      string[0] = ""
-      string[0] = ""
+  sentence = string.split(" ").map do |word|
+    if !word[0].scan(/[aeiouAEIOU]/).empty?
+      word << "way"
+    elsif !word[0].scan(/[bcdfghjklmnpqrstvwxyz]/).empty? && !word[1].scan(/[aeiou]/).empty?
+      word << word[0]
+      word[0] = ""
+      word << "ay"
+    elsif !word[0].scan(/[bcdfghjklmnpqrstvwxyz]/).empty? && !word[1].scan(/[bcdfghjklmnpqrstvwxyz]/).empty? && !word[2].scan(/[bcdfghjklmnpqrstvwxyz]/).empty?
+      word << word[0]
+      word << word[1]
+      word << word[2]
+      word << "ay"
+      word[0] = ""
+      word[0] = ""
+      word[0] = ""
+    elsif !word[0].scan(/[bcdfghjklmnpqrstvwxyz]/).empty? && !word[1].scan(/[bcdfghjklmnpqrstvwxyz]/).empty?
+      word << word[0]
+      word << word[1]
+      word << "ay"
+      word[0] = ""
+      word[0] = ""
 
     end
     string
